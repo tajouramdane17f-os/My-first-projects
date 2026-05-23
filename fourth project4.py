daa07972e6a92725e66d2bd6d49282c734944ce1
Ramdane Faiz Tadj Eddine <tajouramdane17f@gmail.com>
2026-05-23 23:44:22 +0100

    Delete fourth project4.py

--- fourth project4.py
+++
@@ -1,6 +0,0 @@
-import string
-import secrets
-
-chqracters = string .ascii_letters + string.digits + string.punctuation
-password = ''.join(secrets.choice(chqracters) for i in range(24))
-print("your new password is: " + password)
